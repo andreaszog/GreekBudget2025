8ddc8fa152ec4d6a518f63d29fbc1150c36568e9
nnikolopoulou <t8240095@aueb.gr>
2025-11-20 19:30:35 +0200

    new insert class DemandAmount

---
+++ greekbudget2025/src/main/java/gr/greekbudget/DemandAmount.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DemandAmount {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ποιας κατηγορίας τιμή θέλετε να αλλάξετε;")
+    public boolean TypeCategory = false;
+    While (TypeCategory == false) {
+        String category = scanner.nextLine();
+        if (category == exit) {
+        system.exit(0);
+        }
+        public boolean SuccessOrNo(boolean TypeCategory) {
+            for (i = 1; i = ..., i++) {
+                if (...[i] == category) {
+                    TypeCategory = true;
+                    return TypeCategory;
+                    }
+            }
+        }
+        if (TypeCategory == false) {
+            System.out.println("Δώσατε κατηγορία που δεν υπάρχει, παρακαλώ πληκτρολογείστε ξανά.")
+        }
+    }
+}

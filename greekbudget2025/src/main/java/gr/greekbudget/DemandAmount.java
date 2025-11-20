a003d132373f89992defa12fcb1975df7b1fc277
nnikolopoulou <t8240095@aueb.gr>
2025-11-20 19:49:26 +0200

    Μερική ολοκλήρωση κλάσης

--- greekbudget2025/src/main/java/gr/greekbudget/DemandAmount.java
+++ greekbudget2025/src/main/java/gr/greekbudget/DemandAmount.java
@@ -10,7 +10,7 @@ public class DemandAmount {
         system.exit(0);
         }
         public boolean SuccessOrNo(boolean TypeCategory) {
-            for (i = 1; i = ..., i++) {
+            for (i = 1; i = length..., i++) {
                 if (...[i] == category) {
                     TypeCategory = true;
                     return TypeCategory;
@@ -18,7 +18,20 @@ public class DemandAmount {
             }
         }
         if (TypeCategory == false) {
-            System.out.println("Δώσατε κατηγορία που δεν υπάρχει, παρακαλώ πληκτρολογείστε ξανά.")
+            System.out.println("Δώσατε κατηγορία που δεν υπάρχει, παρακαλώ πληκτρολογείστε ξανά.");
+        }
+    }
+    public double newAmount;
+    While (TypeAmount == false) {
+        double newAmount = scanner.nextDouble();
+        public boolean ValueControl (boolean TypeAmount, double newAmount) throws ArithmeticException {
+            if (newAmount < 0) {
+                throw new ArithmeticException("Η τιμή πρέπει να είναι μεγαλύτερη του 0, παρακαλώ ξαναδώσε τιμή.");
+            } else {
+                TypeAmount = true;
+            }
+            return TypeValue;
         }
     }
 }
+scanner.close();
